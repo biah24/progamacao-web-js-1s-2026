588f4f5453ea362829c928044e6fceeb8cde9011
biah24 <bianca20062406@gmail.com>
2026-03-10 13:14:32 -0300

    Add Atividade2

---
+++ atividade2/matrizTransposta.js
@@ -0,0 +1,6 @@
+let matriz = [1, 3.14, -7, 99, 505.67];
+    
+console.log(matriz);
+console.log(`numeros[0]=${numeros[0]}`); //Usando índice para acessar
+console.log(`numeros[3]=${numeros[3]}`); //Começa no 0
+console.log(typeof numeros);
